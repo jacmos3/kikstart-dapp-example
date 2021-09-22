982263c694ed7f4ef786d9495190054f1f83dec1
jacmos3 <j.moscioni@semproxlab.it>
2021-09-22 19:23:11 +0200

    styling adjustements

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -42,7 +42,7 @@ class RequestIndex extends Component{
         <h3>Request List</h3>
         <Link route ={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button primary> Add Requests! </Button>
+            <Button primary floated="right" style={{marginBottom:10}}> Add Requests! </Button>
           </a>
          </Link>
          <Table>
@@ -61,6 +61,7 @@ class RequestIndex extends Component{
             {this.renderRows()}
           </Body>
         </Table>
+        <div>Found {this.props.requestCount} requests.</div>
       </Layout>
     );
   }
